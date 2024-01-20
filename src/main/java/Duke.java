fd380505e4873038d341f3e62b1d99712661b8c3
wjunjie01 <junjiework48@gmail.com>
2024-01-20 15:56:18 +0800

    Replace Duke with Janet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //ASCII art
+        String logo ="       __                 __ \n" +
+                "      / /___ _____  ___  / /_\n" +
+                " __  / / __ `/ __ \\/ _ \\/ __/\n" +
+                "/ /_/ / /_/ / / / /  __/ /_  \n" +
+                "\\____/\\__,_/_/ /_/\\___/\\__/  \n" +
+                "                             \n";
+
+        System.out.println("Hello from" + "\n" + logo);
+
+        System.out.println("Hi! I'm Janet, your personal assistant.");
+        System.out.println("What is your request?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye!");
+
     }
 }
