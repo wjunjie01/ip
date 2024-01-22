7dd460a39bb2364bfc6f628efe9afbda9687b9f8
cheeseong2001 <cheeseong2001@gmail.com>
2024-01-22 23:52:07 +0800

    Increment Level-0 Rename Duke to CheeseBot Add Greeting Add Exit

---
+++ src/main/java/CheeseBot.java
@@ -0,0 +1,13 @@
+public class CheeseBot {
+    public static void main(String[] args) {
+        String greeting = "----------------------------------\n" +
+                "Hello! I'm CheeseBot\n" +
+                "What can I do for you?\n" +
+                "----------------------------------";
+        System.out.println(greeting);
+
+        String farewell = "Bye. Hope to see you again soon!\n" +
+                "----------------------------------";
+        System.out.println(farewell);
+    }
+}
