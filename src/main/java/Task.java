59bb4c12ecabc26fcccc4482538352a66600d3ff
cheeseong2001 <cheeseong2001@gmail.com>
2024-01-29 03:28:48 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String taskName;
+    protected boolean isTaskDone;
+
+    public Task(String taskName) {
+        setTaskName(taskName);
+        setTaskDone(false);
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isTaskDone() {
+        return isTaskDone;
+    }
+
+    public void setTaskDone(boolean taskDone) {
+        isTaskDone = taskDone;
+    }
+
+    public String getStatusIcon() {
+        return (isTaskDone ? "X" : " ");
+    }
+}
