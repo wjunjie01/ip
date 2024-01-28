cfd6685f5657f2424fb932fb8ca0f7c248c58e66
cheeseong2001 <cheeseong2001@gmail.com>
2024-01-29 00:38:48 +0800

    Add echo

--- src/main/java/CheeseBot.java
+++ src/main/java/CheeseBot.java
@@ -1,13 +1,27 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
 public class CheeseBot {
     public static void main(String[] args) {
-        String greeting = "----------------------------------\n" +
-                "Hello! I'm CheeseBot\n" +
-                "What can I do for you?\n" +
-                "----------------------------------";
+        String greeting = "\t----------------------------------\n" +
+                "\tHello! I'm CheeseBot\n" +
+                "\tWhat can I do for you?\n" +
+                "\t----------------------------------";
         System.out.println(greeting);
 
-        String farewell = "Bye. Hope to see you again soon!\n" +
-                "----------------------------------";
-        System.out.println(farewell);
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("\t----------------------------------");
+            System.out.println("\t" + input);
+            System.out.println("\t----------------------------------");
+            input = in.nextLine();
+        }
+
+        String farewell = "\t----------------------------------\n" +
+                "\tBye. Hope to see you again soon!\n" +
+                "\t----------------------------------";
+        System.out.print(farewell);
     }
 }
