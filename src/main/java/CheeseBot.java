6c05e8bb6827e160f2701b163f97148ae8e86c68
cheeseong2001 <cheeseong2001@gmail.com>
2024-01-29 01:10:54 +0800

    Add "add" and "list" features

--- src/main/java/CheeseBot.java
+++ src/main/java/CheeseBot.java
@@ -2,6 +2,25 @@ import javax.sound.midi.SysexMessage;
 import java.util.Scanner;
 
 public class CheeseBot {
+    private static int numberOfTasks = 0;
+    private static String[] inputStore = new String[100];
+    public static void listItems() {
+        String output;
+        System.out.println("\t----------------------------------");
+        for (int i = 0; i < numberOfTasks; i++) {
+            output = "\t" + (i + 1) + ". " + inputStore[i];
+            System.out.println(output);
+        }
+        System.out.println("\t----------------------------------");
+    }
+
+    public static void addItems(String input) {
+        inputStore[numberOfTasks] = input;
+        System.out.println("\t----------------------------------");
+        System.out.println("\tadded: " + input);
+        System.out.println("\t----------------------------------");
+        numberOfTasks++;
+    }
     public static void main(String[] args) {
         String greeting = "\t----------------------------------\n" +
                 "\tHello! I'm CheeseBot\n" +
@@ -9,13 +28,15 @@ public class CheeseBot {
                 "\t----------------------------------";
         System.out.println(greeting);
 
-        String input;
         Scanner in = new Scanner(System.in);
-        input = in.nextLine();
+        String input = in.nextLine();
+
         while (!input.equals("bye")) {
-            System.out.println("\t----------------------------------");
-            System.out.println("\t" + input);
-            System.out.println("\t----------------------------------");
+            if (input.equals("list")) {
+                listItems();
+            } else {
+                addItems(input);
+            }
             input = in.nextLine();
         }
 
