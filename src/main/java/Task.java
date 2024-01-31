3bdbbb61b30bc5c3e6e580b1347fc5802a30854d
wjunjie01 <junjiework48@gmail.com>
2024-01-31 12:19:42 +0800

    Add ToDo, Deadline, Event class
    This is done using the inheritance function from the superclass Task.
    Override in the function toString has been implemented to differentiate
    the outputs between the classes.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
 public class Task{
+    protected static int taskCount = 0;
     protected String description;
     protected boolean isDone;
 
@@ -6,17 +7,63 @@ public class Task{
     public Task(String description){
         this.description = description;
         this.isDone = false;
+        taskCount += 1;
     }
     public String getStatus(){
         return (isDone ? "X": " ");
     }
+
+    public static int getTaskCount() {
+        return taskCount;
+    }
+
     public void markTask(){
         this.isDone = true;
     }
     public void unmarkTask(){
         this.isDone = false;
     }
-    public String getTaskInfo(){
+    @Override
+    public String toString(){
         return "[" + getStatus() + "] " + this.description;
+    }
+}
+
+//Inheritance and Polymorphism
+class Deadline extends Task{
+
+    protected String by;
+    public Deadline(String description, String by){
+        super(description);
+        this.by = by;
+    }
+    @Override
+    public String toString() {
+        return "[D]"+ super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Todo extends Task{
+
+    public Todo(String description){
+        super(description);
+    }
+    @Override
+    public String toString() {
+        return "[T]"+ super.toString();
+    }
+}
+
+class Event extends Task{
+    protected String from;
+    protected String to;
+    public Event(String description, String from, String to){
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+    @Override
+    public String toString() {
+        return "[E]"+ super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
