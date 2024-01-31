c84826064aa5f4f5a72898c18867f2a7abb386f0
wjunjie01 <junjiework48@gmail.com>
2024-01-31 13:38:28 +0800

    Add record function for the different task type: Todo, Deadline, Event
    Able to display the task type along with its status.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 //Coding standard is according to: https://se-education.org/guides/conventions/java/basic.html
 
+import java.util.Arrays;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         //ASCII art
-        String logo ="       __                 __ \n" +
+        String LOGO ="       __                 __ \n" +
                 "      / /___ _____  ___  / /_\n" +
                 " __  / / __ `/ __ \\/ _ \\/ __/\n" +
                 "/ /_/ / /_/ / / / /  __/ /_  \n" +
@@ -13,14 +14,13 @@ public class Duke {
 
         String LINE = "---------------";
 
-        System.out.println(logo);
+        System.out.println(LOGO);
         System.out.println("Hi! I'm Janet, your personal assistant.");
         System.out.println("What is your request?");
         System.out.println(LINE);
 
         Scanner in = new Scanner(System.in);
         Task[]list = new Task[100];
-        int taskCount = 0;
         String input;
 
         while (true){
@@ -29,39 +29,63 @@ public class Duke {
                 break;
             }
 
-            if (input.split(" ")[0].equals("mark")){
+            else if (input.split(" ")[0].equals("mark")){
                 String taskNumDone = input.split(" ")[1];
                 int markIndex = Integer.parseInt(taskNumDone) - 1;
                 list[markIndex].markTask();
                 System.out.println("Well done! This task is now done: ");
-                System.out.println(list[markIndex].getTaskInfo());
+                System.out.println(list[markIndex].toString());
                 System.out.println(LINE);
-                continue;
             }
 
-            if (input.split(" ")[0].equals("unmark")){
+            else if (input.split(" ")[0].equals("unmark")){
                 String taskNumDone = input.split(" ")[1];
                 int markIndex = Integer.parseInt(taskNumDone) - 1;
                 list[markIndex].unmarkTask();
                 System.out.println("Boo! This task is not done yet! ");
-                System.out.println(list[markIndex].getTaskInfo());
+                System.out.println(list[markIndex].toString());
                 System.out.println(LINE);
-                continue;
             }
 
-            if (input.equals("list")){
+            else if (input.equals("list")){
                 System.out.println("The tasks in your list are: ");
-                for (int i = 0; i < taskCount; i++){
-                    System.out.println((i+1) + "." + list[i].getTaskInfo());
+                for (int i = 0; i < Task.getTaskCount(); i++){
+                    System.out.println((i+1) + "." + list[i].toString());
                 }
                 System.out.println(LINE);
             }
-            else{
-                System.out.println("added: " + input);
+
+            else{ //Adds a new task - todo,deadline, event
+                String[] inputArray= input.split(" ");
+                String taskType = inputArray[0];
+                String inputDescription = input.substring(taskType.length() + 1);
+                String taskDescription = "";
+
+                switch (taskType) {
+                case "todo":
+                    taskDescription = inputDescription;
+                    list[Task.getTaskCount()] = new Todo(taskDescription);
+                    break;
+                case "deadline":
+                    int slashIndex = inputDescription.indexOf(" /by ");
+                    taskDescription = inputDescription.substring(0, slashIndex);
+                    String by = inputDescription.substring(slashIndex + 5);
+                    list[Task.getTaskCount()] = new Deadline(taskDescription, by);
+                    break;
+
+                case "event":
+                    int fromSlash = inputDescription.indexOf(" /from ");
+                    int toSlash = inputDescription.indexOf(" /to ");
+                    taskDescription = inputDescription.substring(0, fromSlash);
+                    String from = inputDescription.substring(fromSlash + 7, toSlash);
+                    String to = inputDescription.substring(toSlash + 5);
+                    list[Task.getTaskCount()] = new Event(taskDescription, from, to);
+                    break;
+                }
+
+                System.out.println("Task added: " + taskDescription);
+                System.out.println("You now have " + Task.getTaskCount() + " task(s) in the list.");
                 System.out.println(LINE);
-                //add a new task to the list by use of task constructor
-                list[taskCount] = new Task(input);
-                taskCount += 1;
             }
         }
 
