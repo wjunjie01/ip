787fe4035efd8ccf0a096ee4c0b82a5a92bad8d2
cheeseong2001 <cheeseong2001@gmail.com>
2024-02-04 19:24:13 +0800

    Add "help" function

--- src/main/java/CheeseBot.java
+++ src/main/java/CheeseBot.java
@@ -89,7 +89,8 @@ public class CheeseBot {
     }
 
     private static boolean isValidSingleWordCommand(String input) {
-        if (input.equals("list") || input.equals("bye")) { // for case of single word commands with no space
+        // for case of single word commands with no space
+        if (input.equals("list") || input.equals("bye") || input.equals("help")) {
             return true;
         } else { // for commands where >1 arguments are needed
             System.out.println("\tWrong command usage!");
@@ -311,11 +312,44 @@ public class CheeseBot {
         case "event":
             addTask(arguments);
             break;
+        case "help":
+            printCommandList();
         }
     }
 
+    private static void printCommandList() {
+        System.out.println("\tAvailable commands: list, mark, unmark, todo, deadline, event, help, bye\n");
+        System.out.println("\tCommand 'list' usage: list\n" +
+                "\tFunction: Lists all recorded tasks.\n");
+
+        System.out.println("\tCommand 'mark' usage: mark <task number>\n" +
+                "\tFunction: Marks <task number> as completed.\n");
+
+        System.out.println("\tCommand 'unmark' usage: unmark <task number>\n" +
+                "\tFunction: Marks <task number> as not completed.\n");
+
+        System.out.println("\tCommand 'todo' usage: todo <task name>\n" +
+                "\tFunction: Adds a to do task with no deadline.\n");
+
+        System.out.println("\tCommand 'deadline' usage: deadline <task name> /by <deadline>\n" +
+                "\tFunction: Adds a to do task with a deadline.\n");
+
+        System.out.println("\tCommand 'event' usage: event <event name> /from <start time> /to <end time>\n" +
+                "\tFunction: Adds an event with the start and end timings.\n");
+
+        System.out.println("\tCommand 'help' usage: help\n" +
+                "\tFunction: Lists the available command, along with its usage and functionality.\n");
+
+        System.out.println("\tCommand 'bye' usage: bye\n" +
+                "\tFunction: Ends session with CheeseBot.");
+
+        System.out.println("\t-------------------------------------------------------------------");
+
+
+    }
+
     private static String getInput(Scanner in) {
-        String inputPrompt = "\tPlease input your desired action: ";
+        String inputPrompt = "\tPlease input your desired action or type 'help' for a list of commands: ";
         System.out.println(inputPrompt);
         String input = in.nextLine().strip();
         System.out.println("\t-------------------------------------------------------------------");
