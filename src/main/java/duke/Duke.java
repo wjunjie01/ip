70aec53d900bf2c035e2515407a23662443ce5ee
wjunjie01 <junjiework48@gmail.com>
2024-02-16 16:43:50 +0800

    Add save file and load from file function
    File path is given by data/duke.txt

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,15 +2,30 @@ package duke;
 
 import duke.command.Command;
 import duke.parser.Parser;
+import duke.storage.Storage;
 import duke.task.TaskList;
 import duke.ui.Ui;
+
+import java.io.IOException;
 
 public class Duke {
     public static boolean isExit;
     public TaskList taskList;
+    public Storage storage;
     public Duke(){
         isExit = false;
         taskList = new TaskList();
+
+        String filePath = "data/duke.txt";
+        storage = new Storage(filePath);
+
+        try {
+            taskList = storage.loadTaskList();
+        } catch (DukeException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     public static void main(String[] args) {
         Duke duke = new Duke();
@@ -21,6 +36,7 @@ public class Duke {
                 String input = Ui.readUserInput();
                 Command command = Parser.parse(input, duke.taskList);
                 command.execute(input, duke.taskList);
+                duke.storage.saveTask(duke.taskList);
             } catch (DukeException e) {
                 Ui.printLine();
                 System.out.println(e.getMessage());
--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,5 +1,8 @@
 package duke.task;
 
+import duke.Duke;
+import duke.DukeException;
+
 public class Task {
     protected static int taskCount = 0;
     protected String description;
@@ -30,7 +33,41 @@ public class Task {
     public String toString(){
         return "[" + getStatus() + "] " + this.description;
     }
+
     public String toFileFormat() {
         return " | " + (this.isDone? "1" : "0") + " | " + this.description;
+    }
+
+    public static Task loadFromFile(String line) throws DukeException {
+        String[] taskDescArray = line.split(" \\| ");
+        String taskType = taskDescArray[0];
+        boolean isDone = taskDescArray[1].equals("1");
+        String taskDescription = taskDescArray[2];
+
+        Task task;
+        switch (taskType) {
+        case "T":
+            task = new Todo(taskDescription);
+            break;
+        case "D":
+            String by = taskDescArray[3];
+            task = new Deadline(taskDescription, by);
+            break;
+        case "E":
+            String eventFromTo = taskDescArray[3];
+            String[] eventFromToArray = eventFromTo.split("-");
+            String from = eventFromToArray[0];
+            String to = eventFromToArray[1];
+
+            task = new Event(taskDescription, from, to);
+            break;
+        default:
+            throw new DukeException("Error file format is incorrect and unable to load! ");
+        }
+
+        if (isDone) {
+            task.markTask();
+        }
+        return task;
     }
 }
